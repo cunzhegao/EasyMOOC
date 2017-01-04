47a85f506462a908773c136fb13e10af6eca285e
Kubrick gao <kubrick.gao@jodoinc.com>
2017-01-04 10:24:59 +0800

    Initial Commit

---
+++ EasyMOOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyMOOC
+//
+//  Created by 高存折 on 2017/1/4.
+//  Copyright © 2017年 高存折. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
