f70ceabfbc7e44da98712bc48561d5aefe6b2334
Kubrick Gao <xiongididyi@gmail.com>
2017-01-13 13:06:41 +0800

    add register page

--- EasyMOOC/LoginViewController.swift
+++ EasyMOOC/LoginViewController.swift
@@ -22,15 +22,34 @@ class LoginViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var register: UIButton!
 
+    //MARK: -Login and Register
     @IBAction func login(_ sender: Any) {
         
         if !isStudent && !isTeacher {
             Constant.aler(with: "请选择你的身份", title: "提示")
+        }else if userName.text == "" || userPwd.text == "" {
+            Constant.aler(with: "账号/密码不能为空", title: "提示")
+        }else {
+            LCUser.logIn(username: userName.text!, password: userPwd.text!) { (result) in
+                switch result {
+                    case .success(_):
+                        Constant.aler(with: "登陆成功", title: "")
+                        break
+                    case .failure(let err):
+                        Constant.aler(with: "登录失败 \(err)", title: "")
+                }
+            }
         }
     }
     @IBAction func reigister(_ sender: Any) {
         
+        self.navigationController?.navigationBar.isHidden = false
+        let rvc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "register")
+        rvc.title = "用户注册"
+        self.navigationController?.pushViewController(rvc, animated: true)
     }
+    
+    //MARK: -Turn the button green when user select identity
     @IBAction func selectStudent(_ sender: UIButton) {
         student.backgroundColor = Constant.selectedGreen
         isStudent = true
@@ -45,13 +64,16 @@ class LoginViewController: UIViewController,UITextFieldDelegate {
         
     }
     
+    //MARK: -Set up UI
     override func viewDidLoad() {
         super.viewDidLoad()
         
 
-        self.view.alpha = 0.9
+        self.navigationController?.navigationBar.isHidden = true
+        self.navigationController?.toolbar.isHidden = true
         userName.delegate = self
         userPwd.delegate = self
+        userPwd.isSecureTextEntry = true
         userName.placeholder = "账号..."
         userPwd.placeholder  = "密码..."
         //make the button round
@@ -77,7 +99,7 @@ class LoginViewController: UIViewController,UITextFieldDelegate {
 
     }
     
-
+    //MARK: -TextField Delegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         if textField == userName {
